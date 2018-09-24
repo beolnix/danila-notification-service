ebeb55ffa3beb2fb6724aa945dcf541631185141
Danila <danila.atmakin@auto1.com>
2018-09-24 17:33:54 +0200

    implement of get notifications by device name method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+#[macro_use(bson, doc)]
+extern crate bson;
+extern crate mongodb;
+
 #[macro_use]
 extern crate serde_derive;
 
@@ -10,6 +14,10 @@ use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 
+use mongodb::{Client, ClientOptions, ThreadedClient};
+use mongodb::db::ThreadedDatabase;
+
+use bson::Bson;
 
 const PHRASE: &str = "Hello, World!";
 
@@ -18,30 +26,66 @@ struct DANotificationResponse {
     message_num: i32
 }
 
-fn dispatch(_req: Request<Body>) -> Response<Body> {
+#[derive(Serialize, Deserialize, Debug)]
+struct DANotificationRequest {
+    device_name: String
+}
+
+fn dispatch(_req: Request<Body>, client: std::sync::Arc<mongodb::ClientInner>) -> Response<Body> {
     if _req.uri() != "/danila-skill" {
-        return processNotificaitonRequest(_req);
+        return processNotificaitonRequest(_req, client);
     } else {
-        return processAlexaSkillRequest(_req);
+        return processAlexaSkillRequest(_req, client);
     }
 }
 
-fn processNotificaitonRequest(_req: Request<Body>) -> Response<Body> {
-    let notRsp = DANotificationResponse{ message_num: 1 };
-    let jsonRsp = serde_json::to_string(&notRsp).unwrap();
+fn processNotificaitonRequest(_req: Request<Body>, client: std::sync::Arc<mongodb::ClientInner>) -> Response<Body> {
+    let (parts, body) = _req.into_parts();
+    let uri = parts.uri;
+    let device = str::replace(uri.query().unwrap(), "device=", "");
 
-//    let response = Response::new(Body::from(jsonRsp));
+    println!("DEBUG: received notifications request for device: {}", device);
 
-    let errorDescription = "not found";
-    let response = Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(Body::from(errorDescription)).unwrap();
+    let coll = client.db("danila_app").collection("notifications");
+
+    let doc = doc! {
+        "device": device
+    };
+
+    let mut cursor = coll.find(Some(doc.clone()), None)
+        .ok().expect("failed to find notifications for device");
+
+    let item = cursor.next();
+
+    let result = match item {
+        Some(ref test_doc) => match test_doc {
+            Ok(ref found_doc) => match found_doc.get("count") {
+                Some(&Bson::String(ref count)) => Some(count),
+                _ => None
+            },
+            _ => None
+        },
+        _ => None
+    };
+
+    let response: Response<Body> = match result {
+        Some(count) => Response::builder()
+                .status(StatusCode::OK)
+                .body(Body::from(format!("{{ \"count\": {} }}", count)))
+                .unwrap(),
+        None => {
+            let errorDescription = "not found";
+            return Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::from(errorDescription)).unwrap();
+        }
+    };
 
     return response;
 }
 
-fn processAlexaSkillRequest(_req: Request<Body>) -> Response<Body> {
-    return Response::builder()
+fn processAlexaSkillRequest(_req: Request<Body>, client: std::sync::Arc<mongodb::ClientInner>) -> Response<Body> {
+    Response::builder()
         .status(StatusCode::NOT_IMPLEMENTED)
         .body(Body::empty())
         .unwrap()
@@ -49,16 +93,18 @@ fn processAlexaSkillRequest(_req: Request<Body>) -> Response<Body> {
 
 fn main() {
 
-    // This is our socket address...
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let client = Client::connect("localhost", 27017)
+        .expect("Failed to initialize standalone client.");
 
-    // A `Service` is needed for every connection, so this
-    // creates on of our `hello_world` function.
-    let new_svc = || {
-        // service_fn_ok converts our function into a `Service`
-        service_fn_ok(dispatch)
+
+    let new_svc = move || {
+        let _client = client.clone();
+        service_fn_ok( move |req: Request<Body>| {
+            dispatch(req, _client.clone())
+        })
     };
 
+    let addr = ([127, 0, 0, 1], 3000).into();
     let server = Server::bind(&addr).serve(new_svc).map_err(|e| {
         eprintln!("server error: {}", e)
     });
